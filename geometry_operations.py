feeb771edf201204a2ecd81cc2e9bb623bacc768
Ramson23 <nikita.lukashov2001@mail.ru>
2023-04-24 21:22:54 +0300

    add new function: dist from point to line

--- geometry_operations.py
+++ geometry_operations.py
@@ -23,3 +23,10 @@ def get_angle(x_1, y_1, x_2, y_2):
 
 def get_dist(x_1, y_1, x_2, y_2):
     return math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+
+def get_dist_point_line(pointX,pointY,lineX1,lineY1,lineX2,lineY2):
+    a=lineY2-lineY1
+    b=lineX1-lineX2
+    c=lineX2*lineY1-lineX1*lineY2
+    dis=(math.fabs(a*pointX+b*pointY+c))/(math.pow(a*a+b*b,0.5))
+    return dis
